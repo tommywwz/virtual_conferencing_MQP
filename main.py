0ba0f02ea0448d4887937bf1130c32d4bba7434b
tommywwz <wangweizhe2001@gmail.com>
2022-09-05 23:48:58 -0400

    Img stacked

--- main.py
+++ main.py
@@ -5,6 +5,23 @@ import cvzone
 from cvzone.SelfiSegmentationModule import SelfiSegmentation
 import os
 
+frames = np.empty(2, dtype=object)
+success = [False, False]
+
+
+def ctlThread():
+    name = "Video"
+    cv2.namedWindow(name)
+    while True:
+        f0 = frames[0]
+        f1 = frames[1]
+        if not (f0 is None) and not (f1 is None):
+
+            imgStacked = cvzone.stackImages([f0, f1], 2, 1)
+            cv2.imshow(name, imgStacked)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
 class camThread(threading.Thread):
@@ -20,16 +37,18 @@ class camThread(threading.Thread):
 
 
 def camPreview(previewName, camID, segmentor):
-    cv2.namedWindow(previewName)
+    # cv2.namedWindow(previewName)
     cam = cv2.VideoCapture(camID, cv2.CAP_DSHOW)
     cam.set(3, 426)  # width
     cam.set(4, 240)  # height
 
     while True:
-        success, frame = cam.read()
-        frameOut = segmentor.removeBG(frame, (255, 0, 0), threshold=0.9)
-        if success:
-            cv2.imshow(previewName, frameOut)
+
+        success[camID], frame = cam.read()
+        frames[camID] = segmentor.removeBG(frame, (255, 0, 0), threshold=0.9)
+
+        # if success:
+        #     cv2.imshow(previewName, frameOut)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     print("1Failed to open " + previewName)
@@ -37,8 +56,10 @@ def camPreview(previewName, camID, segmentor):
     cv2.destroyWindow(previewName)
 
 
+thread0 = threading.Thread(target=ctlThread)
 thread1 = camThread("Camera 0", 0)
 thread2 = camThread("Camera 1", 1)
+thread0.start()
 thread1.start()
 thread2.start()
 
