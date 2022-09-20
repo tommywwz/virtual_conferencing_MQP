57eb679eccc93d692095ab8d43c47d02011e966e
MingZH077 <mzhao2@wpi.edu>
2022-09-19 20:18:39 -0400

    add edgeDetect
    add PY file detectEdge

---
+++ detectEdge.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import pyautogui as pg
+
+image = cv2.imread("single_user.jpg")
+grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(grey, (3, 3), 0)
+edged = cv2.Canny(blurred, 150, 160)
+line_image = np.copy(image) * 0
+lines = cv2.HoughLinesP(edged, 1, np.pi/180, 15, np.array([]), 100, 20)
+
+for line in lines:
+    for x1, y1, x2, y2 in line:
+        cv2.line(line_image, (x1, y1), (x2, y2), (0, 255, 255), 2)
+
+line_image = cv2.addWeighted(image, 1, line_image, 1, 0)
+
+cv2.imshow("Original image", image)
+cv2.imshow("Edged image", edged)
+cv2.imshow("Lined image", line_image)
+cv2.waitKey(0)
