20ce2eef1da559646583b2d67afec77df2c6b105
tommywwz <wangweizhe2001@gmail.com>
2022-09-26 01:44:15 -0400

    Create edge_detection.py
    add horizontal line detection solution

---
+++ edge_detection.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import pyautogui as pg
+
+cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cam.set(3, 640)  # width
+cam.set(4, 360)  # height
+# image = cv2.imread("vid/single_user.jpg")
+old_line = np.zeros((1, 4), dtype=np.int32)
+while cam.isOpened():
+
+    success, image = cam.read()
+
+    if success:
+        image = cv2.rotate(image, cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
+        grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(grey, (3, 3), 0)
+        edged = cv2.Canny(blurred, 150, 160)
+        # line_image = np.copy(image) * 0
+        lines = cv2.HoughLinesP(edged, 1, np.pi / 180, 15, np.array([]), 90, 0)
+        line_image = image.copy()
+
+        if lines is not None:
+            print(lines)
+            for line in lines:
+                for x1, y1, x2, y2 in line:
+                    if np.abs(y1 - y2) < 25:
+                        old_line = line
+
+                        cv2.line(line_image, (x1, y1), (x2, y2), (0, 255, 255), 2)
+
+        else:
+            for x1, y1, x2, y2 in old_line:
+                cv2.line(line_image, (x1, y1), (x2, y2), (0, 255, 255), 2)
+
+        # line_image = cv2.addWeighted(image, 1, line_image, 1, 0)
+        cv2.imshow("Original image", image)
+        cv2.imshow("Edged image", edged)
+        cv2.imshow("Lined image", line_image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
