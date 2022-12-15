86fa615c83d3b67fd9c750cef90f839c19595998
tommywwz <wangweizhe2001@gmail.com>
2022-12-15 00:25:21 -0500

    renamed autoresize.py

--- AutoResize.py
+++ AutoResize.py
@@ -1,15 +1,16 @@
 import cv2
 import mediapipe as mp
 import time
-DEBUG = True
+import numpy as np
 
 
-class autoResize:
+class AutoResize:
     def __init__(self):
         self.mp_facedetector = mp.solutions.face_detection
         self.mp_draw = mp.solutions.drawing_utils
+        self.ref_FIFO = []
 
-    def bound_n_resize(self, frame, ref_w, detection_confid=0.7):
+    def bound_n_resize(self, frame, ref_w, detection_confid=0.7, FIFO_len = 5):
 
         with self.mp_facedetector.FaceDetection(min_detection_confidence=detection_confid) as face_detection:
 
@@ -55,8 +56,14 @@ class autoResize:
 
                     box_w = bBox.width*w
                     ref_ratio = ref_w/box_w
-                    adjust_w = round(w*ref_ratio)
-                    adjust_h = round(h*ref_ratio)
+                    if len(self.ref_FIFO) >= FIFO_len:
+                        self.ref_FIFO.pop(0)
+
+                    self.ref_FIFO.append(ref_ratio)
+                    avg_ratio = np.average(self.ref_FIFO)
+
+                    adjust_w = round(w*avg_ratio)
+                    adjust_h = round(h*avg_ratio)
 
                     print("here" + str(adjust_w))
                     frame = cv2.resize(frame, (adjust_w, adjust_h))
@@ -71,9 +78,9 @@ class autoResize:
 
         return frame
 
-
+DEBUG = True
 if DEBUG:
-    auto_rsz = autoResize()
+    auto_rsz = AutoResize()
 
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
