004b1e615ee8f699c444e14989f0a6bc9921dabc
tommywwz <wangweizhe2001@gmail.com>
2022-12-14 23:35:52 -0500

    improved resize n boundary detection accuracy

--- autoResize.py
+++ autoResize.py
@@ -1,79 +1,81 @@
 import cv2
 import mediapipe as mp
 import time
-import numpy as np
-
 DEBUG = True
 
 
-def bound_n_resize(frame, reference):
-    import mediapipe as mp
-    mp_facedetector = mp.solutions.face_detection
-    mp_draw = mp.solutions.drawing_utils
+class autoResize:
+    def __init__(self):
+        self.mp_facedetector = mp.solutions.face_detection
+        self.mp_draw = mp.solutions.drawing_utils
 
-    with mp_facedetector.FaceDetection(min_detection_confidence=0.7) as face_detection:
+    def bound_n_resize(self, frame, ref_w, detection_confid=0.7):
 
-        # Boundary Warning flags
-        bound_warn = [0, 0, 0, 0]
-        # Convert the BGR image to RGB
-        frame = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
-        frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+        with self.mp_facedetector.FaceDetection(min_detection_confidence=detection_confid) as face_detection:
 
-        # Process the image and find faces
-        results = face_detection.process(frame)
+            # Boundary Warning flags
+            bound_warn = [0, 0, 0, 0]
+            # Convert the BGR image to RGB
+            frame = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
+            frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
 
-        # Convert the image color back so it can be displayed
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            # Process the image and find faces
+            results = face_detection.process(frame)
 
-        if results.detections:
-            for id, detection in enumerate(results.detections):
-                mp_draw.draw_detection(frame, detection)
-                #print(id, detection)
+            # Convert the image color back so it can be displayed
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-                bBox = detection.location_data.relative_bounding_box
+            if results.detections:
+                for id, detection in enumerate(results.detections):
+                    if DEBUG: self.mp_draw.draw_detection(frame, detection)
 
-                h, w, c = frame.shape
+                    bBox = detection.location_data.relative_bounding_box
 
-                boundBox = int(bBox.xmin * w), int(bBox.ymin * h), int(bBox.width * w), int(bBox.height * h)
-                print(boundBox)
+                    h, w, c = frame.shape
 
-                # Boundary Warning
-                if boundBox[0] < 20:
-                    bound_warn[0] = 1
-                    cv2.line(frame, (0, 0), (0, 640), (0, 0, 255), 5)
-                if boundBox[0] > 340:
-                    bound_warn[1] = 1
-                    cv2.line(frame, (480, 0), (480, 640), (0, 0, 255), 5)
-                if boundBox[1] < 300:
-                    bound_warn[2] = 1
-                    cv2.line(frame, (0, 0), (480, 0), (0, 0, 255), 5)
-                if boundBox[1] > 400:
-                    bound_warn[3] = 1
-                    cv2.line(frame, (0, 640), (480, 640), (0, 0, 255), 5)
+                    boundBox = int(bBox.xmin * w), int(bBox.ymin * h), int(bBox.width * w), int(bBox.height * h)
+                    # store the boundary information into a tuple with (left most, highest, right most, lowest) location
+                    print(boundBox)
 
-                print(bound_warn)  # boundary warning: [Left, Right, Up, Down], {0} if no warning
+                    # Boundary Warning
+                    if bBox.xmin < 0:  # left bound touched
+                        bound_warn[0] = 1
+                        cv2.line(frame, (0, 0), (0, h), (0, 0, 255), 5)
+                    if bBox.xmin+bBox.width > 1:  # right bound touched
+                        bound_warn[1] = 1
+                        cv2.line(frame, (w, 0), (w, h), (0, 0, 255), 5)
+                    if bBox.ymin+bBox.height > 1:  # lower bound touched
+                        bound_warn[2] = 1
+                        cv2.line(frame, (0, h), (w, h), (0, 0, 255), 5)
+                    if bBox.ymin < 0:  # upper bound touched
+                        bound_warn[3] = 1
+                        cv2.line(frame, (0, 0), (w, 0), (0, 0, 255), 5)
 
-                if (boundBox[3] - reference) > 0.15*reference:
-                    w = round(0.9*w)
-                    h = round(0.9*h)
-                    frame = cv2.resize(frame, (w, h))
-                if (reference - boundBox[3]) > 0.15*reference:
-                    w = round(1.1 * w)
-                    h = round(1.1 * h)
-                    frame = cv2.resize(frame, (w, h))
+                    print(bound_warn)  # boundary warning: [Left, Right, Down, Up], {0} if no warning
 
-    return frame
+                    box_w = bBox.width*w
+                    ref_ratio = ref_w/box_w
+                    adjust_w = round(w*ref_ratio)
+                    adjust_h = round(h*ref_ratio)
 
+                    print("here" + str(adjust_w))
+                    frame = cv2.resize(frame, (adjust_w, adjust_h))
+                    # if (boundBox[3] - reference) > 0.15*reference:
+                    #     w = round(0.9*w)
+                    #     h = round(0.9*h)
+                    #     frame = cv2.resize(frame, (w, h))
+                    # if (reference - boundBox[3]) > 0.15*reference:
+                    #     w = round(1.1 * w)
+                    #     h = round(1.1 * h)
+                    #     frame = cv2.resize(frame, (w, h))
 
-
-
-
-
-
+        return frame
 
 
 if DEBUG:
-    cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+    auto_rsz = autoResize()
+
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
     while cap.isOpened():
         success, image = cap.read()
@@ -81,12 +83,8 @@ if DEBUG:
         start = time.time()
 
         if success:
-            output = bound_n_resize(image, 120)
+            output = auto_rsz.bound_n_resize(image, 100)
             raw = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
-
-
-        end = time.time()
-        totalTime = end - start
 
         cv2.imshow("raw", raw)
         cv2.imshow("test", output)
