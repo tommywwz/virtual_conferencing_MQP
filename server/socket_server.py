87a0644d3c641ca4000ec84749dd273f6b7237bf
tommywwz <wangweizhe2001@gmail.com>
2023-01-19 23:27:48 -0500

    add UDP server debugging

--- server/socket_server.py
+++ server/socket_server.py
@@ -3,6 +3,7 @@
 import cv2
 import socket
 import base64
+import imutils
 
 PORT = 9999
 BUFF_SIZE = 65536
@@ -21,17 +22,20 @@ socket_addr = (host_ip, PORT)
 server_sock.bind(socket_addr)
 print('Listening at: ', socket_addr)
 
-vid = cv2.VideoCapture(3, cv2.CAP_DSHOW)
+vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 vid.set(3, 640)  # width
 vid.set(4, 360)  # height
 while True:
     msg, client_addr = server_sock.recvfrom(BUFF_SIZE)
-    print('GOT CONNECTION FROM: ', client_addr)
+    print('GOT CONNECTION FROM: ', client_addr, ' says: ', msg.decode('utf-8'))
     while vid.isOpened():
         success, frame = vid.read()
         if success:
+            print("frame length " + str(frame.size))
             encoded, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 80])
+            print("\tbuffer length "+str(len(buffer)))
             message = base64.b64encode(buffer)
+            print("\t\tmessage length "+str(len(message)))
             server_sock.sendto(message, client_addr)
 
             cv2.imshow('Transitting Video', frame)
