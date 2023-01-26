f709f788a797620a6d13b0184d7316b0ccb86507
tommywwz <wangweizhe2001@gmail.com>
2023-01-26 15:02:58 -0500

    adding audio function

--- client/socket_client.py
+++ client/socket_client.py
@@ -3,14 +3,20 @@ import cv2
 import pickle
 import socket
 import struct
+import threading
+import pyshine as ps
 from wheels import edge_detection
 from wheels.Frame import Frame
+
+IF_QUIT = False
 
 buff_4K = 4*1024
 
 vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 vid.set(3, 640)  # width
 vid.set(4, 360)  # height
+PORT = 9999
+HOST_IP = '192.168.1.3'  # paste your server ip address here
 
 ed = edge_detection.EdgeDetection()
 camID = socket.gethostbyname(socket.gethostname())
@@ -18,62 +24,71 @@ print(camID)
 frameClass = Frame(camID)
 
 
-# create socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_ip = '192.168.1.3'  # paste your server ip address here
-port = 9999
-client_socket.connect((host_ip, port))  # a tuple
-data = b""
-payload_size = struct.calcsize("Q")
+def video_stream():
+    # create socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    client_socket.connect((HOST_IP, PORT))  # a tuple
+
+    while vid.isOpened():
+        success, frame = vid.read()
+        if success:
+            rsz_image = frame.copy()  # manipulate raw frame here
+            edge = ed.process_frame(rsz_image, threshold=100)
+            a, b = edge
+            if a is not None and b is not None:
+                h, w, c = rsz_image.shape
+                cv2.line(rsz_image, (0, round(b)), (w, round((w * a + b))), (0, 255, 0), 2)
+                # cv2.imshow("test"+str(camID), resized_frame)
+            else:
+                edge = (0, 0)
+            frameClass.updateFrame(image=rsz_image, edge_line=edge)  # update edge information
+
+            pickled_frame = pickle.dumps(frameClass)
+
+            # data length followed by serialized frame object
+            msg = struct.pack("Q", len(pickled_frame)) + pickled_frame
+            client_socket.sendall(msg)
+
+            cv2.imshow('Transitting Video', rsz_image)
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                global IF_QUIT
+                IF_QUIT = True
+                client_socket.close()
+                break
 
 
-while vid.isOpened():
-    success, frame = vid.read()
-    if success:
-        rsz_image = frame.copy()  # manipulate raw frame here
-        edge = ed.process_frame(rsz_image, threshold=100)
-        a, b = edge
-        if a is not None and b is not None:
-            h, w, c = rsz_image.shape
-            cv2.line(rsz_image, (0, round(b)), (w, round((w * a + b))), (0, 255, 0), 2)
-            # cv2.imshow("test"+str(camID), resized_frame)
-        else:
-            edge = (0, 0)
-        frameClass.updateFrame(image=rsz_image, edge_line=edge)  # update edge information
+def audio_stream():
+    # https://pyshine.com/Socket-Programming-send-receive-live-audio/
+    # CONSIDER TO USE WebRTC
+    # https://www.100ms.live/blog/webrtc-python-react-app
+    audio, context = ps.audioCapture(mode='send')
 
-        pickled_frame = pickle.dumps(frameClass)
+    # create socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        # data length followed by serialized frame object
-        msg = struct.pack("Q", len(pickled_frame)) + pickled_frame
-        client_socket.sendall(msg)
+    client_socket.connect((HOST_IP, PORT-1))  # a tuple
 
-        cv2.imshow('Transitting Video', rsz_image)
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
+    while True:
+        frame = audio.get()
+
+        a = pickle.dumps(frame)
+        message = struct.pack("Q", len(a)) + a
+        client_socket.sendall(message)
+
+        if IF_QUIT:
             client_socket.close()
             break
 
 
-# while True:
-#     while len(data) < payload_size:
-#         packet = client_socket.recv(buff_4K)  # 4K
-#         if not packet: break
-#         data += packet
-#     packed_msg_size = data[:payload_size]
-#     data = data[payload_size:]
-#     msg_size = struct.unpack("Q", packed_msg_size)[0]
-#
-#     while len(data) < msg_size:
-#         data += client_socket.recv(buff_4K)
-#     frame_data = data[:msg_size]
-#     data = data[msg_size:]
-#     frame = pickle.loads(frame_data)
-#     cv2.imshow("RECEIVING VIDEO", frame)
-#     key = cv2.waitKey(1) & 0xFF
-#     if key == ord('q'):
-#         break
-# client_socket.close()
+thread0 = threading.Thread(target=video_stream)
+thread1 = threading.Thread(target=audio_stream)
+thread0.start()
+thread1.start()
+print("starting threads")
+thread0.join()
+thread1.join()
 
-
-
+print("All threads are terminated")
 
--- server/socket_server.py
+++ server/socket_server.py
@@ -1,16 +1,19 @@
 # reference: https://pyshine.com/Socket-programming-and-openc/
+# require soundfile, sounddevice
 import cv2
 import pickle
 import socket
 import struct
 import threading
+import pyshine as ps
 from wheels.Frame import Frame
 
 PORT = 9999
 buff_4K = 4 * 1024
+HOST_IP = '192.168.1.3'
 
 
-def client_joined(client_socket, client_addr):
+def video_client(client_socket, client_addr):
     data = b""
     payload_size = struct.calcsize("Q")
     windowName = str(client_addr)
@@ -18,7 +21,8 @@ def client_joined(client_socket, client_addr):
     while True:
         while len(data) < payload_size:
             packet = client_socket.recv(buff_4K)  # 4K
-            if not packet: break
+            if not packet:
+                break
             data += packet
         packed_msg_size = data[:payload_size]  # extracting the packet size information
         data = data[payload_size:]  # extract the img data from the rest of the packet
@@ -39,15 +43,37 @@ def client_joined(client_socket, client_addr):
             break
 
 
-def start_listening():
+def audio_client(client_socket, client_addr):
+    audio, context = ps.audioCapture(mode='get')
+    data = b""
+    payload_size = struct.calcsize("Q")
+    while True:
+        while len(data) < payload_size:
+            packet = client_socket.recv(4 * 1024)  # 4K
+            if not packet: break
+            data += packet
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("Q", packed_msg_size)[0]
+
+        while len(data) < msg_size:
+            data += client_socket.recv(4 * 1024)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+        frame = pickle.loads(frame_data)
+        audio.put(frame)
+
+    client_socket.close()
+
+
+def video_manage():
     server_sock = socket.socket(socket.AF_INET,
                                 socket.SOCK_STREAM)
     host_name = socket.gethostname()
     host_ips = socket.gethostbyname_ex(host_name)
-    host_ip = '192.168.1.3'
-    print('host ip: ' + host_ip)
+    print('host ip: ' + HOST_IP)
 
-    socket_addr = (host_ip, PORT)
+    socket_addr = (HOST_IP, PORT)
 
     # socket bind
     server_sock.bind(socket_addr)
@@ -59,55 +85,44 @@ def start_listening():
     while True:
         # waiting from client connection and create a thread for it
         client_socket, client_addr = server_sock.accept()
-        print('GOT NEW CONNECTION FROM: ', client_addr)
+        print('GOT NEW VIDEO CONNECTION FROM: ', client_addr)
         if client_socket:
-            newClientThread = threading.Thread(target=client_joined, args=(client_socket, client_addr))
-            newClientThread.start()
+            newClientVidThread = threading.Thread(target=video_client, args=(client_socket, client_addr))
+            newClientVidThread.start()
             print("starting thread for client:", client_addr)
 
 
-thread0 = threading.Thread(target=start_listening)
-thread0.start()
+def audio_manage():
+    # https://pyshine.com/Socket-Programming-send-receive-live-audio/
+    # CONSIDER TO USE WebRTC
+    # https://www.100ms.live/blog/webrtc-python-react-app
+    server_sock = socket.socket(socket.AF_INET,
+                                socket.SOCK_STREAM)
+    host_name = socket.gethostname()
+    host_ips = socket.gethostbyname_ex(host_name)
+    print('host ip: ' + HOST_IP)
+
+    socket_addr = (HOST_IP, PORT-1)
+
+    # socket bind
+    server_sock.bind(socket_addr)
+
+    # socket listen
+    server_sock.listen(5)
+    print('Listening at: ', socket_addr)
+
+    while True:
+        # waiting from client connection and create a thread for it
+        client_socket, client_addr = server_sock.accept()
+        print('GOT NEW AUDIO CONNECTION FROM: ', client_addr)
+        if client_socket:
+            newClientAudThread = threading.Thread(target=audio_client, args=(client_socket, client_addr))
+            newClientAudThread.start()
+            print("starting thread for client:", client_addr)
+
+
+video_man = threading.Thread(target=video_manage)
+audio_man = threading.Thread(target=audio_manage)
+video_man.start()
 print("starting thread0")
 
-
-# while True:
-#     client_socket, client_addr = server_sock.accept()
-#     print('GOT CONNECTION FROM: ', client_addr)
-#
-#     while client_socket:
-#         while len(data) < payload_size:
-#             packet = client_socket.recv(buff_4K)  # 4K
-#             if not packet: break
-#             data += packet
-#         packed_msg_size = data[:payload_size]
-#         data = data[payload_size:]
-#         msg_size = struct.unpack("Q", packed_msg_size)[0]
-#
-#         while len(data) < msg_size:
-#             data += client_socket.recv(buff_4K)
-#         frame_data = data[:msg_size]
-#         data = data[msg_size:]
-#         frame = pickle.loads(frame_data)
-#         cv2.imshow("RECEIVING VIDEO", frame)
-#         key = cv2.waitKey(1) & 0xFF
-#         if key == ord('q'):
-#             client_socket.close()
-#             break
-
-
-# if client_socket:
-#     vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-#     while vid.isOpened():
-#         success, frame = vid.read()
-#         if success:
-#             pickled_frame = pickle.dumps(frame)
-#
-#             # data length followed by serialized frame object
-#             msg = struct.pack("Q", len(pickled_frame))+pickled_frame
-#             client_socket.sendall(msg)
-#
-#             cv2.imshow('Transitting Video', frame)
-#             key = cv2.waitKey(1) & 0xFF
-#             if key == ord('q'):
-#                 client_socket.close()
