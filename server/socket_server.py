5ad769629785d394e2eca2796a43f3a6e7bb45e2
tommywwz <wangweizhe2001@gmail.com>
2023-02-02 22:40:15 -0500

    Revert "UDP video"
    This reverts commit aa5e192a48ad4551370a71f51276ba122969be34.

--- server/socket_server.py
+++ server/socket_server.py
@@ -5,7 +5,6 @@ import pickle
 import socket
 import struct
 import threading
-import numpy as np
 import pyaudio
 import wave
 from wheels.Frame import Frame
@@ -21,103 +20,72 @@ sample_width = 2
 
 if __name__ == '__main__':
 
-    # audio_clients = {}
-    # # Create a lock to protect the clients dictionary
-    # lock = threading.Lock()
+    audio_clients = {}
+    # Create a lock to protect the clients dictionary
+    lock = threading.Lock()
 
 
-    # def video_client(client_socket, client_addr):
-    #     data = b""
-    #     payload_size = struct.calcsize("Q")
-    #     windowName = str(client_addr)
-    #     cv2.namedWindow(windowName)
-    #     while True:
-    #         while len(data) < payload_size:
-    #             packet = client_socket.recv(buff_4K)  # 4K
-    #             if not packet:
-    #                 break
-    #             data += packet
-    #         packed_msg_size = data[:payload_size]  # extracting the packet size information
-    #
-    #         if not packed_msg_size:  # check if client has lost connection
-    #             client_socket.close()
-    #             cv2.destroyWindow(windowName)
-    #             print("Lost connection from client:", client_addr)
-    #             break
-    #
-    #         data = data[payload_size:]  # extract the img data from the rest of the packet
-    #         msg_size = struct.unpack("Q", packed_msg_size)[0]
-    #
-    #         while len(data) < msg_size:
-    #             # keep loading the data until the entire data received
-    #             data += client_socket.recv(buff_4K)
-    #         frame_data = data[:msg_size]
-    #         data = data[msg_size:]
-    #         frameClass = pickle.loads(frame_data)
-    #         frame = frameClass.img
-    #         cv2.imshow(windowName, frame)
-    #         key = cv2.waitKey(1) & 0xFF
-    #         if key == ord('q'):
-    #             client_socket.close()
-    #             cv2.destroyWindow(windowName)
-    #             break
-    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server.bind((HOST_IP, PORT))
+    def video_client(client_socket, client_addr):
+        data = b""
+        payload_size = struct.calcsize("Q")
+        windowName = str(client_addr)
+        cv2.namedWindow(windowName)
+        while True:
+            while len(data) < payload_size:
+                packet = client_socket.recv(buff_4K)  # 4K
+                if not packet:
+                    break
+                data += packet
+            packed_msg_size = data[:payload_size]  # extracting the packet size information
+
+            if not packed_msg_size:  # check if client has lost connection
+                client_socket.close()
+                cv2.destroyWindow(windowName)
+                print("Lost connection from client:", client_addr)
+                break
+
+            data = data[payload_size:]  # extract the img data from the rest of the packet
+            msg_size = struct.unpack("Q", packed_msg_size)[0]
+
+            while len(data) < msg_size:
+                # keep loading the data until the entire data received
+                data += client_socket.recv(buff_4K)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+            frameClass = pickle.loads(frame_data)
+            frame = frameClass.img
+            cv2.imshow(windowName, frame)
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                client_socket.close()
+                cv2.destroyWindow(windowName)
+                break
 
 
-    class ClientThread(threading.Thread):
-        def __init__(self, addr, client):
-            threading.Thread.__init__(self)
-            self.addr = addr
-            self.client = client
+    def video_manage():
+        server_sock = socket.socket(socket.AF_INET,
+                                    socket.SOCK_STREAM)
+        host_name = socket.gethostname()
+        host_ips = socket.gethostbyname_ex(host_name)
+        print('host ip: ' + HOST_IP)
 
-        def run(self):
-            frame = b''
-            expected_size = 0
-            while True:
-                data, recv_addr = self.client.recvfrom(640 * 480 * 3)
-                if expected_size == 0:
-                    expected_size = int(data[:10].decode('utf-8').strip())
-                    data = data[10:]
-                frame += data
-                if len(frame) == expected_size:
-                    frame = np.fromstring(frame, dtype=np.uint8).reshape(480, 640, 3)
-                    cv2.imshow(str(self.addr), frame)
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
-                        break
-                    frame = b''
-                    expected_size = 0
+        socket_addr = (HOST_IP, PORT)
 
+        # socket bind
+        server_sock.bind(socket_addr)
 
-    while True:
-        _, address = server.recvfrom(640 * 480 * 3)
-        client = ClientThread(address, server)
-        client.start()
+        # socket listen
+        server_sock.listen(5)
+        print('Listening at: ', socket_addr)
 
-    # def video_manage():
-    #     server_sock = socket.socket(socket.AF_INET,
-    #                                 socket.SOCK_STREAM)
-    #     host_name = socket.gethostname()
-    #     host_ips = socket.gethostbyname_ex(host_name)
-    #     print('host ip: ' + HOST_IP)
-    #
-    #     socket_addr = (HOST_IP, PORT)
-    #
-    #     # socket bind
-    #     server_sock.bind(socket_addr)
-    #
-    #     # socket listen
-    #     server_sock.listen(5)
-    #     print('Listening at: ', socket_addr)
-    #
-    #     while True:
-    #         # waiting from client connection and create a thread for it
-    #         client_socket, client_addr = server_sock.accept()
-    #         print('GOT NEW VIDEO CONNECTION FROM: ', client_addr)
-    #         if client_socket:
-    #             newClientVidThread = threading.Thread(target=video_client, args=(client_socket, client_addr))
-    #             newClientVidThread.start()
-    #             print("starting thread for client:", client_addr)
+        while True:
+            # waiting from client connection and create a thread for it
+            client_socket, client_addr = server_sock.accept()
+            print('GOT NEW VIDEO CONNECTION FROM: ', client_addr)
+            if client_socket:
+                newClientVidThread = threading.Thread(target=video_client, args=(client_socket, client_addr))
+                newClientVidThread.start()
+                print("starting thread for client:", client_addr)
 
 
 
