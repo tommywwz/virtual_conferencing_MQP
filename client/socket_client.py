aa5e192a48ad4551370a71f51276ba122969be34
tommywwz <wangweizhe2001@gmail.com>
2023-02-02 20:29:00 -0500

    UDP video

--- client/socket_client.py
+++ client/socket_client.py
@@ -9,55 +9,68 @@ from wheels.Frame import Frame
 
 IF_QUIT = False
 
-buff_4K = 4 * 1024
 
 cam = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 cam.set(3, 640)  # width
 cam.set(4, 360)  # height
 PORT = 9999
-HOST_IP = '192.168.1.13'  # paste your server ip address here
-
+HOST_IP = '192.168.1.3'  # paste your server ip address here
+socket_addr = (HOST_IP, PORT+1)
 
 if __name__ == '__main__':
     ed = edge_detection.EdgeDetection()
     camID = socket.gethostbyname(socket.gethostname())
     print(camID)
     frameClass = Frame(camID)
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while True:
+        ret, frame = cam.read()
+        frame = cv2.resize(frame, (640, 480))
+        data = frame.tostring()
+        size = str(len(data)).zfill(10)
+        for i in range(0, len(data), 512):
+            client.sendto((size + data[i:i + 512]).encode('utf-8'), socket_addr)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    def video_stream():
-        # create socket
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cam.release()
+    client.close()
+    cv2.destroyAllWindows()
 
-        client_socket.connect((HOST_IP, PORT))  # a tuple
-
-        while cam.isOpened():
-            success, frame = cam.read()
-            if success:
-                rsz_image = frame.copy()  # manipulate raw frame here
-                edge = ed.process_frame(rsz_image, threshold=100)
-                a, b = edge
-                if a is not None and b is not None:
-                    h, w, c = rsz_image.shape
-                    cv2.line(rsz_image, (0, round(b)), (w, round((w * a + b))), (0, 255, 0), 2)
-                    # cv2.imshow("test"+str(camID), resized_frame)
-                else:
-                    edge = (0, 0)
-                frameClass.updateFrame(image=rsz_image, edge_line=edge)  # update edge information
-
-                pickled_frame = pickle.dumps(frameClass)
-
-                # data length followed by serialized frame object
-                msg = struct.pack("Q", len(pickled_frame)) + pickled_frame
-                client_socket.sendall(msg)
-
-                cv2.imshow('Transitting Video', rsz_image)
-                key = cv2.waitKey(1) & 0xFF
-                if key == ord('q'):
-                    global IF_QUIT
-                    IF_QUIT = True
-                    cam.release()
-                    client_socket.close()
-                    break
+    # def video_stream():
+    #     # create socket
+    #     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #
+    #     client_socket.connect((HOST_IP, PORT))  # a tuple
+    #
+    #     while cam.isOpened():
+    #         success, frame = cam.read()
+    #         if success:
+    #             rsz_image = frame.copy()  # manipulate raw frame here
+    #             edge = ed.process_frame(rsz_image, threshold=100)
+    #             a, b = edge
+    #             if a is not None and b is not None:
+    #                 h, w, c = rsz_image.shape
+    #                 cv2.line(rsz_image, (0, round(b)), (w, round((w * a + b))), (0, 255, 0), 2)
+    #                 # cv2.imshow("test"+str(camID), resized_frame)
+    #             else:
+    #                 edge = (0, 0)
+    #             frameClass.updateFrame(image=rsz_image, edge_line=edge)  # update edge information
+    #
+    #             pickled_frame = pickle.dumps(frameClass)
+    #
+    #             # data length followed by serialized frame object
+    #             msg = struct.pack("Q", len(pickled_frame)) + pickled_frame
+    #             client_socket.sendall(msg)
+    #
+    #             cv2.imshow('Transitting Video', rsz_image)
+    #             key = cv2.waitKey(1) & 0xFF
+    #             if key == ord('q'):
+    #                 global IF_QUIT
+    #                 IF_QUIT = True
+    #                 cam.release()
+    #                 client_socket.close()
+    #                 break
 
 
 # # Create a socket object
@@ -99,13 +112,12 @@ if __name__ == '__main__':
 #     # Close the socket
 #     s.close()
 
-    thread0 = threading.Thread(target=video_stream)
+    # thread0 = threading.Thread(target=video_stream)
     # thread1 = threading.Thread(target=audio_stream)
-    thread0.start()
+    # thread0.start()
     # thread1.start()
-    print("starting threads")
-    thread0.join()
+    # print("starting threads")
+    # thread0.join()
     # thread1.join()
-
-    print("All threads are terminated")
+    # print("All threads are terminated")
     exit(0)
