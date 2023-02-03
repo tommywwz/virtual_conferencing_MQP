3ad2a53b814d9edf76f39d705d598f8e5c63c8f3
tommywwz <wangweizhe2001@gmail.com>
2023-02-03 00:41:49 -0500

    enabled multithread audio handler

--- server/server_audio.py
+++ server/server_audio.py
@@ -1,30 +1,42 @@
 import pyaudio
 import socket
+import threading
 from socket_server import HOST_IP, PORT
 
-CHUNK = 1024
+CHUNK = 512
 FORMAT = pyaudio.paInt16
-CHANNELS = 2
+CHANNELS = 1
 RATE = 44100
 
 
-def play_audio(conn):
-    p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    output=True,
-                    frames_per_buffer=CHUNK)
-    while True:
-        data = conn.recv(CHUNK)
-        stream.write(data)
+class ClientHandler(threading.Thread):
+    def __init__(self, addr, client_sock):
+        threading.Thread.__init__(self)
+        self.addr = addr
+        self.client_sock = client_sock
+
+    def run(self):
+        p = pyaudio.PyAudio()
+        stream = p.open(format=FORMAT,
+                        channels=CHANNELS,
+                        rate=RATE,
+                        output=True,
+                        frames_per_buffer=CHUNK)
+        while True:
+            data = self.client_sock.recv(CHUNK)
+            stream.write(data)
 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket_addr = (HOST_IP, PORT+1)
+socket_addr = (HOST_IP, PORT + 1)
 server.bind(socket_addr)
-server.listen(1)
-client, addr = server.accept()
-play_audio(client)
-# play_thread = threading.Thread(target=play_audio, args=(conn,))
-# play_thread.start()
+server.listen(5)
+
+while True:
+    client, client_addr = server.accept()
+    if client:
+        clientThread = ClientHandler(client_addr, client)
+        clientThread.start()
+
+server.close()
+
